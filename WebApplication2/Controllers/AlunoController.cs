24bbec366fb6eae61c47d0534656df3f1d83432e
faggotini11 <goncalomourato1@gmail.com>
2022-07-04 15:04:42 +0100

    Adicionar arquivos de projeto.

---
+++ WebApplication2/Controllers/AlunoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Detalhe()
+        {
+            Aluno alunos = new Aluno()
+            {
+                AlunoId = 200,
+                Nome = "Gonçalo",
+                Apelido = "Mourato",
+                Telefone = "930571912"
+            };
+
+            ViewBag.Nome = alunos.Nome;
+            ViewData["Apelido"] = alunos.Apelido;
+            return View(alunos);
+        }
+
+        [HttpPost]
+        public IActionResult GuardarAluno(Aluno modelo)
+        {
+            return View(modelo);
+        }
+
+    }
+}
---
+++ WebApplication2/Models/Aluno.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2.Models
+{
+    public class Aluno
+    {
+        public int AlunoId { get; set; }
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+        public string Telefone { get; set; }
+
+        public string NomeCompleto => $"{Nome} , {Apelido}";
+    }
+}
